4878f578a1f6b5eae5ebf3eebd7080bc561c87cf
ReAlign <realign@yeah.net>
2023-05-29 19:57:31 +0800

    update img

--- src/pages/Content/modules/index.js
+++ src/pages/Content/modules/index.js
@@ -6,6 +6,38 @@ const loginLiveAdmin = () => {
   }
 };
 
+const removeBingAd = () => {
+  const webUrl = window.location.href;
+  if (webUrl.indexOf('https://cn.bing.com/search') === 0) {
+    const ads = document.querySelectorAll('li.b_ad');
+    if (ads) {
+      [].forEach.call(ads, (ad) => {
+        ad.style.display = 'none';
+      });
+    }
+  }
+};
+
+const removeBaiduAd = () => {
+  const webUrl = window.location.href;
+  if (webUrl.indexOf('https://www.baidu.com/s') === 0) {
+    const ads = document.querySelector('#content_left > div:nth-child(1)');
+    if (ads) {
+      ads.style.display = 'none';
+    }
+  }
+};
+
+const redirectLocalhostHttps = () => {
+  const href = window.location.href;
+  if (href.startsWith('https://localhost')) {
+    window.location.href = `http${href.substring(5)}`;
+  }
+};
+
 export const runModules = () => {
   loginLiveAdmin();
+  removeBingAd();
+  removeBaiduAd();
+  redirectLocalhostHttps();
 };
